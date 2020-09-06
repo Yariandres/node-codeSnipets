463b0721aa7e8bfca179a70fb216a21c6ee933b6
Yari Andres <yari.andres@gmail.com>
2020-09-06 22:37:27 +0200

    adding nodemon to the project

--- assigment-1/routes.js
+++ assigment-1/routes.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-
+const { parse } = require('path');
+debugger;
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-  debugger;
+
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -12,21 +13,36 @@ const requestHandler = (req, res) => {
     return res.end();
   }
 
+  if(url === '/user') {
+    res.write('<html>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
   if(url === '/create-user' && method === 'POST') {
-        
+    const body = [];
+    
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody.split('=', [1]));
+
+    });
+    res.statusCode = 302;
+    res.setHeader('Locoation', '/');
+    res.end();
+
+
     console.log('user has been created');
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body><h1>User has been created </h1></body>');
     res.write('</html>');
-    return res.end();  
-      
+    return res.end();   
   }  
-  res.write('<html>');
-  res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
-  res.write('</html>');
-  res.end();
-  
 }
 
 module.exports = requestHandler;
--- simple-serve/app.js
+++ simple-serve/app.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 const server = http.createServer(routes);
 
 const port = 3000;
+
 server.listen(port);
 
 // More resources

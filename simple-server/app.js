c919edcc14b8e656ec0224e83b95390f31f41d19
Yari Andres <yari.andres@gmail.com>
2020-09-07 20:37:21 +0200

    adding css styling to the application

---
+++ simple-server/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const routes = require('./routes');
+
+const server = http.createServer(routes);
+
+const port = 3000;
+
+server.listen(port);
+
+// More resources
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+
